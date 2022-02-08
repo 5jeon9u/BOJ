72bba3157f782c3677aa2cffeaf377354b37017f
5jeon9u <97647987+5jeon9u@users.noreply.github.com>
2022-02-08 21:06:44 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 72 ms, Memory: 30864 KB -BaekjoonHub

--- 백준/Bronze/10809.알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809.알파벳 찾기/알파벳 찾기.py
@@ -1,11 +1,9 @@
-r1, r2 = ord('a'), ord('z')
-lows = range(r1, r2+1)
-S = list(map(chr, lows))
+S = input()
 
-txt = input()
-
-for char in S:
-    if char in txt:
-        print(txt.index(char), end=' ')
-    else:
-        print(-1, end=' ')
+for i in range(97, 123):
+    for j in range(0, len(S)):
+        num = -1
+        if i == ord(S[j]):
+            num = j
+            break
+    print(num, end=' ')
