c9fcf597433393dccde2bcbb6f297a852bc63d2a
lhynjn9 <97647987+lhynjn9@users.noreply.github.com>
2022-03-14 23:23:09 +0900

    [Silver III] Title: N과 M (3), Time: 2136 ms, Memory: 30860 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+lst = []  # 출력 내용
+
+def perm(start):
+    if len(lst) == M: # 2개의 수만을 취함
+        print(*lst)
+        return
+    else:
+        for i in range(start, N+1):
+            lst.append(i)
+            perm(1)
+            lst.pop()
+
+perm(1)
